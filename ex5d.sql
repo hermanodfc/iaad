cac9b4cd6eb37c6c875232f1bfeb9eee69acf6a1
hermanodfc <33607718+hermanodfc@users.noreply.github.com>
2020-03-10 11:04:20 -0300

    Delete ex5d.sql

--- ex5d.sql
+++
@@ -1,3 +0,0 @@
-SELECT p.numero_pedido, c.nome, v.nome_vendedor FROM pedidos p
-	JOIN clientes c ON (p.codigo_cliente = c.codigo_cliente)
-	JOIN vendedores v ON (p.codigo_vendedor = v.codigo_vendedor)
